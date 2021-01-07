e40837828d3033e581ad1f061c731415e0da777e
cburke8 <61330354+cburke8@users.noreply.github.com>
2021-01-07 16:32:30 -0600

    added load silver to step two

--- uat/step_two/spark_demo_two.py
+++ uat/step_two/spark_demo_two.py
@@ -17,6 +17,8 @@ silver_path = 's3://citi-bike-batch-data-test/silver/'
 gold_path   = 's3://citi-bike-batch-data-test/gold/'
 
 #isolating circular trips
+slvrDF = spark.read.format('parquet').load(silver_path)
+
 slvrDF.createOrReplaceTempView("silver")
 
 # Making DIM_STATION starts:
